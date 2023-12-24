5720b1a7f40a75828bae0ff7a9ef5f13f05d73cd
juni-chen <mail@jun-athletics.blog>
2023-12-24 21:24:56 +0900

    christmas

--- textbooks/urls.py
+++ textbooks/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path, include
 
+app_name = 'textbooks'
+
 urlpatterns = [
     path('', views.index_template, name='index'),
 ]
--- wordquiz/urls.py
+++ wordquiz/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path, include
 
+app_name = 'wordquiz'
+
 urlpatterns = [
     path('', views.quiz_index_template, name='index'),
 ]
